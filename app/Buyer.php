b10c1d37fbc67a878edaf4882d4fc3e164aa3ae5
Andrianaja Ranaivoarivelo <andrianajaranaivoarivelo@MacBook-Air-de-Andrianaja.local>
2019-06-16 23:45:16 +0300

    model implement

--- app/Buyer.php
+++ app/Buyer.php
@@ -4,5 +4,8 @@ namespace App;
 
 class Buyer extends User
 {
-    //
+    public function transations()
+    {
+        return $this->hasMany('App\Transaction', 'buyer_id');
+    }
 }
--- app/Product.php
+++ app/Product.php
@@ -6,5 +6,35 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    const AVAILABLE_PRODUCT = 'available';
+    const UNAVAILABLE_PRODUCT = 'unavailable';
+
+    protected $fillable = [
+        'name',
+        'description',
+        'quantity',
+        'status',
+        'image',
+        'seller_id'
+    ];
+
+    public function isAvailable()
+    {
+        return $this->status == Product::AVAILABLE_PRODUCT;
+    }
+
+    public function seller()
+    {
+        return $this->belongsTo('App\Seller');
+    }
+
+    public function transactions()
+    {
+        return $this->hasMany('App\Transaction');
+    }
+
+    public function categories()
+    {
+        return $this->belongsToMany('App\Category');
+    }
 }
