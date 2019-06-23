9548d810327918d028490aa35310805c6d4b3165
Andrianaja Ranaivoarivelo <andrianajaranaivoarivelo@MacBook-Air-de-Andrianaja.local>
2019-06-23 16:42:45 +0300

    cache

--- app/Traits/ApiResponser.php
+++ app/Traits/ApiResponser.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Collection;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Pagination\LengthAwarePaginator;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Cache;
 trait ApiResponser
 {
     private function successResponse($data, $code)
@@ -33,6 +34,7 @@ trait ApiResponser
         $collection = $this->sortData($collection, $transformer);
         $collection = $this->paginate($collection);
         $collection = $this->transformData($collection, $transformer);
+        $collection = $this->cacheResponse($collection);
         return $this->successResponse($collection, $code);
     }
 
@@ -105,5 +107,21 @@ trait ApiResponser
         $transformation = fractal($data, new $transformer);
 
         return $transformation->toArray();
+    }
+
+    protected function cacheResponse($data)
+    {
+        $url = request()->url();
+        $queryParams = request()->query();
+
+        ksort($queryParams);
+
+        $queryString = http_build_query($queryParams);
+
+        $fullUrl = "{$url}?{$queryString}";
+
+        return Cache::remember($fullUrl, 30/60, function() use ($data) {
+            return $data;
+        });
     }
 }
